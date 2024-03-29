994e06bcb42006be3dc15722fb8a64f98a6bec19
feassis <felipe.assis.2002@gmail.com>
2024-03-29 15:16:05 -0300

    Implementing base of battle visuals

---
+++ Assets/Script/UI/BattleVisuals.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class BattleVisuals : MonoBehaviour
+{
+    [SerializeField] private Slider hpSlider;
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private Animator animator;
+    [SerializeField] private 
+
+    private float curHP;
+    private float maxHP;
+    private int level;
+
+    private const string LEVEL_ABV = "Lvl: ";
+    private const string ATTACK_ANIM_TRIGGER = "attack";
+    private const string IS_HIT_ANIM_TRIGGER = "isHit";
+    private const string IS_DEAD_ANIM_TRIGGER = "isDead";
+
+    private void Start()
+    {
+        SetStartingValues(10, 10, 5);
+    }
+
+    public void SetStartingValues(float currentHP, float maxHP, int level)
+    {
+        curHP = currentHP;
+        this.maxHP = maxHP;
+        this.level = level;
+        hpSlider.maxValue = maxHP;
+        hpSlider.value = currentHP;
+        levelText.text = LEVEL_ABV + level;
+    }
+
+    public void ChangeHealth(float currentHP)
+    {
+        curHP = currentHP;
+
+        if (curHP < 0)
+        {
+            PlayIsDeadAnim();
+            //Destroy(gameObject, 1f);
+        }
+        UpdateHPBar();
+    }
+
+    public void UpdateHPBar()
+    {
+        hpSlider.value = curHP;
+    }
+
+    public void PlayAttackAnim()
+    {
+        animator.SetTrigger(ATTACK_ANIM_TRIGGER);
+    }
+
+    public void PlayIsHitAnim()
+    {
+        animator.SetTrigger(IS_HIT_ANIM_TRIGGER);
+    }
+
+    public void PlayIsDeadAnim()
+    {
+        animator.SetTrigger(IS_DEAD_ANIM_TRIGGER);
+    }
+
+}
